08c3414470ed46940e30484a1336de1e9623813a
BlairWiser <blair.m.wiser@gmail.com>
2015-05-08 16:20:56 -0400

    Added Leiningen project for clojure sandbox experimentation.

---
+++ sandbox/src/sandbox/hello.clj
@@ -0,0 +1,18 @@
+;; To run this file in the REPL use:
+;; (use :reload 'sandbox.hello)
+
+(ns sandbox.hello)
+
+(import '(java.util Random))
+(let [rand (Random.)]
+	(println "Rand = " (. rand nextInt 10)))
+
+;; Accessing static members and methods
+(println "Pi is " Math/PI)
+(println "Sqrt of 9 is " (Math/sqrt 9))
+
+;; Accessing Java's string functions
+(println (.toUpperCase "I am excited!"))
+(println (.indexOf "Let's find the S" "S"))
+
+(println "=================")
