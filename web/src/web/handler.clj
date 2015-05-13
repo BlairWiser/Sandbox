831c7b03875417e3528fad54c5d65388dc0ce8e3
BlairWiser <blair.m.wiser@gmail.com>
2015-05-13 09:22:01 -0400

    Added web clojure project to experiment with web app creation.

---
+++ web/src/web/handler.clj
@@ -0,0 +1,15 @@
+(ns web.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.middleware.json :as middleware]))
+
+(defroutes app-routes
+  (GET "/" [] {:status 200, :body {:stuff "Hello"}})
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+(-> (handler/site app-routes)
+  (middleware/wrap-json-body {:keywords? true}) 
+  (middleware/wrap-json-response)))
