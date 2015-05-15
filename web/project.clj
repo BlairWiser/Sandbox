d5f6361120c4352f8391bd150dd51f2c05245d36
BlairWiser <blair.m.wiser@gmail.com>
2015-05-15 14:03:44 -0400

    Changed handler.clj to use opennlp functionality and to serve front.html. Serving frontl.html. Javascript on front.html not working.

--- web/project.clj
+++ web/project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]]
+                 [ring/ring-json "0.3.1"]
+                 [clojure-opennlp "0.3.3"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler web.handler/app}
   :profiles
--- web/src/web/handler.clj
+++ web/src/web/handler.clj
@@ -2,10 +2,23 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [ring.middleware.json :as middleware]))
+            [ring.middleware.json :as middleware]
+            [ring.util.response :as resp])
+  (:use opennlp.nlp))
+
+;; Start with lein ring server from Desktop/Sandbox/web
+
+(def get-sentences (make-sentence-detector "resources/opennlp/en-sent.bin"))
 
 (defroutes app-routes
-  (GET "/" [] {:status 200, :body {:stuff "Hello"}})
+  (GET "/" [] (resp/resource-response "front.html" {:root "public"}))
+  (POST "/" request
+        (let [sentence (or (get-in request [:body :sentence]) 
+                           (get-in request [:data :sentence])
+                           ("I study quantum physics."))]
+          {:status 200
+           :body {:sentence sentence
+                  :prob (:probabilities (meta (get-sentences sentence)))}}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
